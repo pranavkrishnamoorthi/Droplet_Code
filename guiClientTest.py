8d97f28f879ed5a85cde4d64c630d804e886f7ae
Srisai Nachuri <srisainachuri@berkeley.edu>
2021-02-18 16:34:39 -0800

    Replaced keyboard input with simple gui

--- guiClientTest.py
+++ guiClientTest.py
@@ -7,6 +7,7 @@
 # # 'All done'
 
 from threading import Thread, Event
+import tkinter as tk
 
 msg = 'hi'
 
@@ -21,25 +22,62 @@ def worker(run_event):
             print('eee')
 
 
+window = tk.Tk()
+label = tk.Label(text="Enter a radius value")
+entry = tk.Entry()
+button = tk.Button(text="Set Radius")
+buttonQuit = tk.Button(text="Exit Controller")
+
+
+def radiusClick(event):
+    global msg
+    try:
+        msg = int(entry.get())
+    except:
+        return
+
+
+def quitClick(event):
+    global window
+    window.quit()
+
+
+window.geometry("500x200")
+button.bind('<Button>', radiusClick)
+buttonQuit.bind('<Button>', quitClick)
+label.pack()
+entry.pack()
+button.pack()
+buttonQuit.pack()
+
+
 if __name__ == '__main__':
     run_event = Event()
     run_event.set()
     t1 = Thread(target=worker, args=(run_event,))
     t1.daemon = True
     t1.start()
-    while msg != 'q':
-        try:
-            msg_raw = input()
-            if msg_raw == 'q':
-                run_event.clear()
-                print("normal")
-                break
-            try:
-                msg = int(msg_raw)
-            except:
-                continue
-        except:
-            msg = 'q'
-            run_event.clear()
-            print("hehe")
-            break
+    # while msg != 'q':
+    #     try:
+    #         msg_raw = input()
+    #         if msg_raw == 'q':
+    #             run_event.clear()
+    #             print("normal")
+    #             break
+    #         try:
+    #             msg = int(msg_raw)
+    #         except:
+    #             continue
+    #     except:
+    #         msg = 'q'
+    #         run_event.clear()
+    #         print("hehe")
+    #         break
+
+    try:
+        window.mainloop()
+        run_event.clear()
+    except:
+        run_event.clear()
+        print("hehe")
+    run_event.clear()
